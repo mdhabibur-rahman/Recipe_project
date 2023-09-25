019f728f6a82858d6aabb15141dd3e7b32d516fe
mdhabibur-rahman <145204698+mdhabibur-rahman@users.noreply.github.com>
2023-09-26 00:40:11 +0600

    Add_Aecipe_View_Count

--- vege/models.py
+++ vege/models.py
@@ -5,3 +5,4 @@ class Recipe(models.Model):
     recipe_name = models.CharField(max_length=100)
     recipe_description = models.TextField()
     recipe_image = models.ImageField(upload_to="media/recipe/",max_length=250,null=True,default=None)
+    recipr_view_count = models.IntegerField(default=1)
